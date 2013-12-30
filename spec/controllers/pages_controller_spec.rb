45fa4f2a6b716fd43ed81a4d6f0e94f6c7b39bcf
floolf <flo.bellamine@gmail.com>
2013-12-30 13:27:37 +0100

    fichiers oublies

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,40 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
-  end
+	it "devrait avoir le bon titre" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => " | Accueil")
+    end
+ end 
 
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
       response.should be_success
     end
+	it "devrait avoir le bon titre" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => " | contact")
+    end
   end
-
+  describe "GET 'about'" do
+    it "should work" do
+      get 'about'
+      response.should be_success
+	end
+	it "devrait avoir le bon titre" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => " | about")
+    end
+end
 end
