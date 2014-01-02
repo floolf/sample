3e9f41c1fb6bd7de4432947053f5d95706d011bb
floolf <flo.bellamine@gmail.com>
2014-01-02 23:03:59 +0100

    routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+ it "devrait trouver une page Accueil at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Accueil")
+  end
+
+  it "devrait trouver une page Contact at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "contact")
+  end
+
+  it "should have an About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "about")
+  end
+
+  it "devrait trouver une page Iade at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "help")
+  end
+ 
+end
